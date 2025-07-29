90353e655ed8ffb87d7aa685bf61bf686b52741a
SUBHAMOY SAHA <144066267+Suhbhamoy07@users.noreply.github.com>
2025-07-29 16:52:44 +0530

    Upload Day 4

---
+++ DSA_With_PYTHON/Array/Day_4.py
@@ -0,0 +1,57 @@
+# --------------> Rotate Array <--------------
+
+# Note : Given an unsorted array arr[] , Rotate the array to the left (Counter Clockwise direction) by d steps. Where d is a positive integer. Do the mentioned change in the array in-place
+
+# Example 1: Input: arr[] = {1, 2, 3, 4, 5, 6, 7}, d = 2
+# Output: {3, 4, 5, 6, 7, 1, 2}
+# Explanation: When the rotated by 2 elements, it become {3, 4, 5, 6, 7, 1, 2}
+
+# Example 2: Input: arr[] = {7, 3, 9, 1}, d = 9
+# Output: {3, 9, 1, 7}
+# Explanation: When we rotate 9 times, we'll ger 3, 9, 1, 7 as resultant array
+
+import math
+
+#Function to rotate array
+def RotateArr(arr, d):
+    n = len(arr)
+
+    # Handle the case where d > size of array
+    d %= n
+
+    # Calculate the number of cycles in the rotation 
+    cycles = math.gcd(n, d)
+
+    # Process each cycle
+    for i in range(cycles):
+
+        # Start element of the current cycle
+        startElement = arr[i]
+
+        # Start index of current cycle
+        currIdx = i
+
+        # Rotate elements till we reach the start of cycle
+        while True:
+            nextIdx = (currIdx + d) % n
+
+            if nextIdx == i:
+                break
+
+            # Updata the next index with the current element
+            arr[currIdx] = arr[nextIdx]
+            currIdx = nextIdx
+
+            # Copy the start element of current cycle at the last
+            # index of the cycle
+
+            arr[currIdx] = startElement
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    d = 3
+    RotateArr(arr, d)
+    print("Input Array : ", arr)
+    print("Rotated Array : ", end="")
+    for num in arr:
+        print(num, end=" ")  # Output: 3 4 5 6 7 1 2
